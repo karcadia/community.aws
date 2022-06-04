b8bb23e9099f21328d85c2757226f4b38b4cdabe
karcadia <kymil_arcadia@yahoo.com>
2022-06-04 12:15:37 -0500

    import snake_dict_to_camel_dict

--- plugins/modules/ecs_ecr.py
+++ plugins/modules/ecs_ecr.py
@@ -5,6 +5,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import snake_dict_to_camel_dict
 
 __metaclass__ = type
 
